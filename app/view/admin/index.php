089908a9f2997f1a8eaac20ee428d272e80d31ab
Loïck Virot <virot.loick@gmail.com>
2016-12-22 19:23:07 +0100

    Create bases of admin pages

--- app/view/admin/index.php
+++ app/view/admin/index.php
@@ -1,2 +1,9 @@
-Admin page
-<a href="/auth/logout" class="btn btn-danger">Logout</a>
+<h1>Administration <small><a href="/auth/logout" class="text-danger">Déconnexion</a></small></h1>
+<div class="row">
+    <div class="col-md-6">
+        <h2>Joueurs <small><a href="#"><span class="glyphicon glyphicon-plus"></span>Ajouter un joueur</a></small></h2>
+    </div>
+    <div class="col-md-6">
+        <h2>Matchs <small><a href=""><span class="glyphicon glyphicon-plus"></span>Ajouter un match</a></small></h2>
+    </div>
+</div>
--- app/view/default/footer.php
+++ app/view/default/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
   <p>
-    Created by <a href="http://loickvirot.fr/">Loïck Virot</a>
+    KP-Manager est créé par <a href="http://loickvirot.fr/">Loïck Virot</a> et Julien Deframond pour un projet de l'IUT Informatique de l'Université Toulouse III Paul Sabatier.
   </p>
 </footer>

4f6627c7d8723c26d4fb8ad3a0979e0ece5e2656
Loïck Virot <virot.loick@gmail.com>
2017-01-03 08:39:24 +0100

    Edit footer

--- app/view/default/footer.php
+++ app/view/default/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
   <p>
-    KP-Manager est créé par <a href="http://loickvirot.fr/">Loïck Virot</a> et Julien Deframond pour un projet de l'IUT Informatique de l'Université Toulouse III Paul Sabatier.
+    KP-Manager a été créé par <a href="http://loickvirot.fr/" target="_blank">Loïck Virot</a> et <a href="https://framagit.org/daniel421" target="_blank">Julien Deframond</a> pour un projet de l'IUT Informatique de l'Université Toulouse III Paul Sabatier.
   </p>
 </footer>

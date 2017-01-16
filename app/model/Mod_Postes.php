036e661c9b6f2d9e7c5167f734bff653deb9c2ae
Loïck Virot <virot.loick@gmail.com>
2017-01-16 19:06:05 +0100

    Docker

--- app/model/Mod_Postes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Mod_Postes extends Database
-{
-    public function getPostes()
-    {
-        return $this->select("SELECT * FROM postes");
-    }
-}
--- app/view/home.php
+++
@@ -1,8 +0,0 @@
-<section class="home">
-    <h1>KP-Manager</h1>
-    <p>KP-Manager est un logiciel permettant de faciliter la gestion d'une équipe de Kayak-polo.</p>
-    <p>Ce logiciel est un projet pour l'IUT Informatique de l'Université Toulouse III Paul Sabatier.</p>
-    <h2>Se connecter</h2>
-    <p>Afin d'utiliser le logiciel, il vous faut vous connecter.</p>
-    <a href="/auth" class="btn btn-primary">Connexion</a>
-</section>
